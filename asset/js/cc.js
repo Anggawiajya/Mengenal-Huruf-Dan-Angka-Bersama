33380fc1dc28c70a15076b4320aa55856640562d
Anggawiajya <anggawija1233@gmail.com>
2024-08-27 11:19:02 +0700

    cc.js

--- asset/js/cc.js
+++ asset/js/cc.js
@@ -66,6 +66,15 @@ self.addEventListener('activate', event => {
     );
 });
 
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+    .then(registration => {
+        console.log('Service Worker registered with scope:', registration.scope);
+    }).catch(error => {
+        console.log('Service Worker registration failed:', error);
+    });
+}
+
 //ld
 window.addEventListener('load', function() {
     const loadingScreen = document.getElementById('loading');
